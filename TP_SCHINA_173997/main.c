55473f3b09d5be10710a8481b22efeac24c11788
ViniciusMazzitelli <viniciusmazzitelli2@hotmail.com>
2021-08-14 17:16:09 -0300

    Olá

---
+++ TP_SCHINA_173997/main.c
@@ -0,0 +1,20 @@
+/*
+ * TP_SCHINA_173997.c
+ *
+ * Created: 14/08/2021 16:15:18
+ * Author : User
+ */ 
+
+#include <avr/io.h>
+extern void tarefa();
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		tarefa();
+    }
+}
+
